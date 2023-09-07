8bfdef49406cd97b5533a0715f540ec61613a41b
gmpark <gmpark@pabloair.com>
2023-09-07 17:52:22 +0900

    refactor(Base): Change unused param

--- java/spring/rest-api-sample/src/main/java/sample/api/domain/dto/BaseDTO.java
+++ java/spring/rest-api-sample/src/main/java/sample/api/domain/dto/BaseDTO.java
@@ -2,5 +2,5 @@ package sample.api.domain.dto;
 
 import java.io.Serializable;
 
-public abstract class BaseDTO<T1, T2> implements Serializable {
+public abstract class BaseDTO<E, T extends BaseDTO<E, T>> implements Serializable {
 }
